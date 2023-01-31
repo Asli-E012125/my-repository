fe770d0873a262984994b0286af9f7899968daf6
Asli-E012125 <aslicebe@gmail.com>
2023-01-30 21:40:47 -0500

    email application v1

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+def convert(decimal_num):
+    roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+    num_to_roman = ''
 
+    for i in roman.keys():
+        num_to_roman += roman[i]*(decimal_num//i) 
+        decimal_num %= i
+    return num_to_roman
 @app.route('/', methods=['POST', 'GET'])
 def main_post():
     if request.method == 'POST':
